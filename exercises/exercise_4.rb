192fc4fc440095b8e8869bd3d064284950c8c888
originallykevin <ly1@ualberta.ca>
2022-10-18 02:35:41 +0000

    complete excerise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,21 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create([
+  {name:"Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true},
+  {name:"Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false},
+  {name:"Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true}
+]);
+
+# stores that carry men's apparel
+@mens_stores = Store.where(mens_apparel:true)
+# stores that carry women's apparel and <1M annual revenue
+@womens_stores = Store.where("womens_apparel =true and annual_revenue < 1000000")
+
+for index in @mens_stores do
+  puts "Mens store - name: #{index.name}, annual_revenue: #{index.annual_revenue}" 
+end
+
+for index in @womens_stores do
+  puts "Womens store < 1M - name: #{index.name}, annual_revenue: #{index.annual_revenue}"
+end
